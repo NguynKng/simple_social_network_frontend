11281034e8f69d66ab4721ddd4c48b4e709f962d
NguynKng <nguynkhang2109@gmail.com>
2026-03-12 14:48:02 +0700

    add page3

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { Suspense } from "react";
 import SpinnerLoading from "./components/SpinnerLoading";
 import PageKt from "./pages/PageKt";
 import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
 
 const LazyPage = ({ Element, Layout = MainLayout }) => (
   <Suspense fallback={<SpinnerLoading />}>
@@ -67,6 +69,18 @@ function App() {
           path="/page1"
           element={
             <Page1 />
+          }
+        />
+        <Route
+          path="/page2"
+          element={
+            <Page2 />
+          }
+        />
+        <Route
+          path="/page3"
+          element={
+            <Page3 />
           }
         />
         <Route
